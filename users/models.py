685a8d9d20b538ec35335adda58d799c295f48cc
Your Name <you@example.com>
2026-04-16 17:39:06 +0530

    loginpageupdated

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+
+class CustomUserManager(UserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'SUPERADMIN')
+        return super().create_superuser(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -9,6 +15,8 @@ class User(AbstractUser):
         ("USER", "User"),
     )
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='USER')
+
+    objects = CustomUserManager()
     device_limit = models.IntegerField(default=10, help_text='Max devices this user can create. 0 = unlimited. Only applies to USER role.')
     created_by = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='created_users')
 
